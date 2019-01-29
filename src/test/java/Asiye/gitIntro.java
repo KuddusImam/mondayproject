947e14a6c1f0ebc472867c2203fd4f7db20cef49
“Kuddus <ikuddus@gmail.com>
2019-01-28 23:05:20 -0500

    added code to gitIntro class

--- src/test/java/Asiye/gitIntro.java
+++ src/test/java/Asiye/gitIntro.java
@@ -1,9 +1,23 @@
 package Asiye;
 
+import org.testng.annotations.BeforeClass;
+
 public class gitIntro {
+
 
     public static void main(String[] args) {
         System.out.println("Asiye");
+
+        String word = "Java";
+        System.out.println(word.charAt(0));
+
+        for (int i=0; i<=word.length()-1; i++){
+            System.out.println(word.charAt(i));
+
+        }
     }
+
+
+
 
 }
